77373e01921ba5401921e389d8162bda35c5d824
Justin Patrin <justin@idle-games.com>
2013-02-26 17:35:28 -0800

    readability

--- test/pybitcoin/test_key.py
+++ test/pybitcoin/test_key.py
@@ -3,13 +3,7 @@ import unittest
 from pybitcoin import key
 
 
-class TestAddressMethods(unittest.TestCase):
-    def test_address_from_pk_hash(self):
-        self.assertEquals(
-            key.address_from_pk_hash(
-                '\xe9\x9f\xaa\x1b\x12\x8f\x13w\x8d"#\xa9\xd1\xd3~\x88\x92\x0b]B'),
-            '\x00\xe9\x9f\xaa\x1b\x12\x8f\x13w\x8d"#\xa9\xd1\xd3~\x88\x92\x0b]B\xe6\xf2\xf5Y')
-
+class TestBase58(unittest.TestCase):
     def test_base58_encode_zero(self):
         self.assertEquals(key.base58_encode('\x00'), '1')
 
@@ -29,14 +23,29 @@ class TestAddressMethods(unittest.TestCase):
             key.address_from_pk_hash(
                 '\xe9\x9f\xaa\x1b\x12\x8f\x13w\x8d"#\xa9\xd1\xd3~\x88\x92\x0b]B'))
 
+
+class TestAddressMethods(unittest.TestCase):
+    def test_address_from_pk_hash(self):
+        self.assertEquals(
+            key.address_from_pk_hash(
+                '\xe9\x9f\xaa\x1b\x12\x8f\x13w\x8d"#\xa9\xd1\xd3~\x88\x92\x0b]B'),
+            '\x00\xe9\x9f\xaa\x1b\x12\x8f\x13w\x8d"#\xa9\xd1\xd3~\x88\x92\x0b]B\xe6\xf2\xf5Y')
+
     def test_address_from_pubkey(self):
-        self.assertEquals(key.address_from_pubkey('\x04\x01\x00\xb9\xce\x1a\xcfg\x16\xa2\xd6Os\xb2\xa6%\xf2\xc1\x0c\xe9\x19\xe5(\xa9?`Z\xf1\xd7=M[AY\x99\xff\xde\xf8\x89\xd9m\xccB"\x0e\xd9ys\xb0S{k\x95\xa3\xec\\\xa2\xaf\x96B`\x1fK\x12\xe8'),
-                          '\x00\x87\x8b\xb12\x12\x0c\xcb\x1e\xd0\x7f\x87\xb5\xf6Q6\t\xb1\xff\xe2\xcc\x9f\xb2^\xc1')
+        self.assertEquals(
+            key.address_from_pubkey(
+                '\x04\x01\x00\xb9\xce\x1a\xcfg\x16\xa2\xd6Os\xb2\xa6%\xf2\xc1\x0c\xe9\x19\xe5(\xa9?`Z\xf1\xd7='
+                'M[AY\x99\xff\xde\xf8\x89\xd9m\xccB"\x0e\xd9ys\xb0S{k\x95\xa3\xec\\\xa2\xaf\x96B`\x1fK\x12\xe8'),
+            '\x00\x87\x8b\xb12\x12\x0c\xcb\x1e\xd0\x7f\x87\xb5\xf6Q6\t\xb1\xff\xe2\xcc\x9f\xb2^\xc1')
 
     def test_address_from_compressed_pubkey(self):
-        self.assertEquals(key.address_from_pubkey('\x02\x01\x00\xb9\xce\x1a\xcfg\x16\xa2\xd6Os\xb2\xa6%\xf2\xc1\x0c\xe9\x19\xe5(\xa9?`Z\xf1\xd7=M[A'),
-                          '\x00\xc5\xf0\xf9\xb63\x08+b\xfe\xb5 \xb8\xea<L\x12)\x1c\xaae\xe7\xf6\xf30')
+        self.assertEquals(
+            key.address_from_pubkey(
+                '\x02\x01\x00\xb9\xce\x1a\xcfg\x16\xa2\xd6Os\xb2\xa6%\xf2\xc1\x0c\xe9\x19\xe5(\xa9?`Z\xf1\xd7=M[A'),
+            '\x00\xc5\xf0\xf9\xb63\x08+b\xfe\xb5 \xb8\xea<L\x12)\x1c\xaae\xe7\xf6\xf30')
 
+
+class TestKey(unittest.TestCase):
     def test_decode_privkey(self):
         self.assertEqual(
             key.decode_privkey('5JnKZDMUAddiGgFjWiHNVrX5pxGcEJ1miscs2Xhy7f9BrGffrps'),
@@ -68,12 +77,16 @@ class TestAddressMethods(unittest.TestCase):
     def test_encode_pub(self):
         pub = key.priv_to_pub(
             key.decode_privkey('5JnKZDMUAddiGgFjWiHNVrX5pxGcEJ1miscs2Xhy7f9BrGffrps'))
-        self.assertEquals(key.encode_pub(pub), '04EAB82662C4A329F573E96801CCFCF9337446D2742EFDC5A6E8EA8F617AD0197B387DDFA56684EF2F4E2325F298F5F418ADCB00F560B75F4DEEAF90ABD5A3CEB0'.decode('hex'))
+        self.assertEquals(
+            key.encode_pub(pub),
+            ('04EAB82662C4A329F573E96801CCFCF9337446D2742EFDC5A6E8EA8F617AD0197B387DDFA'
+             '56684EF2F4E2325F298F5F418ADCB00F560B75F4DEEAF90ABD5A3CEB0').decode('hex'))
 
     def test_encode_pub_compressed(self):
         pub = key.priv_to_pub(
             key.decode_privkey('5JnKZDMUAddiGgFjWiHNVrX5pxGcEJ1miscs2Xhy7f9BrGffrps'))
-        self.assertEquals(key.encode_pub_compressed(pub), '02EAB82662C4A329F573E96801CCFCF9337446D2742EFDC5A6E8EA8F617AD0197B'.decode('hex'))
+        self.assertEquals(key.encode_pub_compressed(pub),
+                          '02EAB82662C4A329F573E96801CCFCF9337446D2742EFDC5A6E8EA8F617AD0197B'.decode('hex'))
 
     def test_priv_key_to_address(self):
         self.assertEquals(
