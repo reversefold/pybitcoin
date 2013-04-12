c03acfd89b1bbfb0c52bd439a720b1e869dd316a
Justin Patrin <justin@idle-games.com>
2013-04-12 12:22:01 -0700

    Test the rest of recv_bytes

--- test/pybitcoin/test_io.py
+++ test/pybitcoin/test_io.py
@@ -35,3 +35,38 @@ class TestRecvBytes(mox.MoxTestBase):
         sock.recv(1).AndReturn('b')
         self.mox.ReplayAll()
         self.assertEqual(io.recv_bytes(sock, 2), 'ab')
+
+    def test_socket_error(self):
+        sock = self.mox.CreateMock(socket.socket)
+        self.mox.StubOutWithMock(sock, 'recv')
+        sock.recv(1).AndRaise(socket.error)
+        self.mox.ReplayAll()
+        with self.assertRaises(socket.error):
+            io.recv_bytes(sock, 1)
+
+    def test_connection_reset(self):
+        sock = self.mox.CreateMock(socket.socket)
+        def raise_104(_):
+            err = socket.error()
+            err.errno = 104
+            raise err
+        sock.recv = raise_104
+        self.mox.ReplayAll()
+        with self.assertRaises(io.ConnectionClosedError):
+            io.recv_bytes(sock, 1)
+
+    def test_connection_closed(self):
+        sock = self.mox.CreateMock(socket.socket)
+        self.mox.StubOutWithMock(sock, 'recv')
+        sock.recv(1).AndReturn('')
+        self.mox.ReplayAll()
+        with self.assertRaises(io.ConnectionClosedError):
+            io.recv_bytes(sock, 1)
+
+    def test_impossible_length_error(self):
+        sock = self.mox.CreateMock(socket.socket)
+        self.mox.StubOutWithMock(sock, 'recv')
+        sock.recv(1).AndReturn('ab')
+        self.mox.ReplayAll()
+        with self.assertRaises(io.SocketError):
+            io.recv_bytes(sock, 1)
