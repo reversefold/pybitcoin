173ad309eb257b57a6a2b0286692914385979926
Justin Patrin <justin@idle-games.com>
2013-07-28 14:36:14 -0700

    The beginning of hierarchical deterministic wallets from BIP_0032 (https://en.bitcoin.it/wiki/BIP_0032)

---
+++ pybitcoin/hd_wallet.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python
+from hashlib import sha256, sha512, new as new_hash
+import hmac
+import struct
+
+from pybitcoin.byte_util import base58_encode_checksum, base58_decode_checksum
+from pybitcoin.key import _decode_pub_compressed, encode_pub_compressed, encode_bigint, decode_bigint, priv_to_pub, secp256k1
+
+
+def hmac_sha512(key, msg):
+    return hmac.new(key, msg, sha512)
+
+
+class Error(Exception):
+    pass
+
+
+class HDKey(object):
+    def __init__(
+        self, key, chain_code, version,
+        depth=0, child_number=0, parent_fingerprint='\x00\x00\x00\x00'
+    ):
+        self.version = version
+        self.key = key
+        self.chain_code = chain_code
+        self.depth = depth
+        self.child_number = child_number
+        self.parent_fingerprint = parent_fingerprint
+
+    def fingerprint(self):
+        raise TypeError('unimplemented')
+
+    def serialized_key(self):
+        raise TypeError('unimplemented')
+
+    def serialized(self):
+        return ''.join([
+            self.version,
+            struct.pack('>B', self.depth),
+            self.parent_fingerprint,
+            struct.pack('>L', self.child_number),
+            self.chain_code,
+            self.serialized_key()
+        ])
+
+    def encoded(self):
+        return base58_encode_checksum(self.serialized())
+
+    @classmethod
+    def decode(cls, enc):
+        return cls.unserialize(base58_decode_checksum(enc))
+
+    @classmethod
+    def unserialize(cls, raw):
+        version = raw[:4]
+        #key_id = raw[45]
+        #if key_id == '\x00':
+        #    if version not in HDPrivKey.VERSIONS:
+        #        raise Error('version (%r) does not match key prefix (%r)' % (version, key_id))
+        #elif key_id in ('\x02', '\x03'):
+        #    if version not in HDPubKey.VERSIONS:
+        #        raise Error('version (%r) does not match key prefix (%r)' % (version, key_id))
+        #else:
+        #    raise Error('key_id not recognized (%r)' % key_id)
+        depth = struct.unpack('>B', raw[4:5])[0]
+        parent_fingerprint = raw[5:9]
+        child_number = struct.unpack('>L', raw[9:13])[0]
+        chain_code = raw[13:45]
+        #key = raw[46:]
+        #if key_id == '\x00':
+        #    key = HDPrivKey.unserialize_key(key_id, key)
+        #    return HDPrivKey(key, chain_code, depth, child_number, parent_fingerprint, HDPrivKey.V_T[version])
+        #elif key_id in ('\x02', '\x03'):
+        #    key = HDPubKey.unserialize_key(key_id, key)
+        #    return HDPubKey(key, chain_code, depth, child_number, parent_fingerprint, version == '\x04\x35\x87\xCF')
+        raw_key = raw[45:]
+        #if version in HDPubKey.VERSIONS:
+        #    key = HDPubKey.unserialize_key(raw_key)
+        #    return  HDPubKey(key, chain_code, depth, child_number, parent_fingerprint, HDPubKey.V_T[version])
+        #elif version in HDPrivKey.VERSIONS:
+        #    key = HDPrivKey.unserialize_key(raw_key)
+        #    return  HDPrivKey(key, chain_code, depth, child_number, parent_fingerprint, HDPrivKey.V_T[version])
+        if version in VERSION_KEY_MAP:
+            key_cls = VERSION_KEY_MAP[version]
+            key = key_cls.unserialize_key(raw_key)
+            return key_cls(key, chain_code, depth, child_number, parent_fingerprint, key_cls.V_T[version])
+        else:
+            raise TypeError('version not recognized (%r)' % (version,))
+
+    @classmethod
+    def unserialize_key(cls, key):
+        raise TypeError('unimplemented')
+
+
+class HDPrivKey(HDKey):
+    T_V = {True: '\x04\x35\x83\x94', False: '\x04\x88\xAD\xE4'}
+
+    def __init__(self, key, chain_code, depth=0, child_number=0, parent_fingerprint='\x00\x00\x00\x00', testnet=False):
+        super(HDPrivKey, self).__init__(
+            key, chain_code,
+            self.T_V[testnet],
+            depth, child_number, parent_fingerprint)
+
+    def serialized_key(self):
+        return '\x00' + encode_bigint(self.key)  # should this be encode_bigint?
+
+    def derive_child(self, i):
+        high_bit = i & 0x80000000
+        if high_bit:
+            I = hmac_sha512(self.chain_code, '\x00' + encode_bigint(self.key) + struct.pack('>L', i)).digest()
+        else:
+            I = hmac_sha512(self.chain_code, encode_pub_compressed(priv_to_pub(self.key)) + struct.pack('>L', i)).digest()
+        IL = decode_bigint(I[:32])
+        if IL >= secp256k1.order:
+            raise Error('invalid key')
+        IR = I[32:]
+        ki = (IL + self.key) % secp256k1.order
+        if ki == 0:
+            raise Error('invalid key')
+        ci = IR
+        return HDPrivKey(ki, ci, self.depth + 1, i, self.fingerprint(), self.V_T[self.version])
+
+    def fingerprint(self):
+        return self.pub().fingerprint()
+
+    def pub(self):
+        return HDPubKey(priv_to_pub(self.key), self.chain_code, self.depth, self.child_number, self.parent_fingerprint, self.V_T[self.version])
+
+    @classmethod
+    def unserialize_key(cls, raw):
+        key_type = raw[0]
+        if key_type != '\x00':
+            raise TypeError('key_type should be \\x00, not %r' % (key_type,))
+        return decode_bigint(raw[1:])  # should this be decode_bigint?
+
+
+class HDPubKey(HDKey):
+    T_V = {True: '\x04\x35\x87\xCF', False: '\x04\x88\xB2\x1E'}
+
+    def __init__(self, key, chain_code, depth=0, child_number=0, parent_fingerprint='\x00\x00\x00\x00', testnet=False):
+        super(HDPubKey, self).__init__(
+            key, chain_code,
+            self.T_V[testnet],
+            depth, child_number, parent_fingerprint)
+
+    def serialized_key(self):
+        return encode_pub_compressed(self.key)  # ('\x02' if self.key.x % 2 == 0 else '\x03') + long_to_bytes(self.key.x)  # should this be encode_bigint?
+
+    def fingerprint(self):
+        return new_hash('ripemd160', sha256(self.serialized_key()).digest()).digest()[:4]
+
+    def derive_child(self, i):
+        if i & 0x80000000:
+            raise Error('Invalid i (%r)' % (i,))
+        else:
+            I = hmac_sha512(self.chain_code, encode_pub_compressed(self.key) + struct.pack('>L', i)).digest()
+        IL = decode_bigint(I[:32])
+        if IL >= secp256k1.order:
+            raise Error('invalid key')
+        IR = I[32:]
+
+        Ki = IL * secp256k1.generator + self.key
+        #if Ki == INFINITY:
+        #    raise Error('invalid key')
+        ci = IR
+        return HDPubKey(Ki, ci, self.depth + 1, i, self.fingerprint(), self.V_T[self.version])
+
+    @classmethod
+    def unserialize_key(cls, raw):
+        return _decode_pub_compressed(raw)
+        #key_type = raw[0]
+        #if key_type not in ['\x02', '\x03']:
+        #    raise TypeError('key_type should be \\x02 or \\x03, not %r' % (key_type,))
+        #x = bytes_to_long(raw[1:])  # or should this be decode_bigint?
+
+
+VERSION_KEY_MAP = {}
+for cls in [HDPubKey, HDPrivKey]:
+    cls.V_T = {v: k for k, v in cls.T_V.iteritems()}
+    cls.VERSIONS = cls.T_V.values()
+    for version in cls.VERSIONS:
+        VERSION_KEY_MAP[version] = cls
+
