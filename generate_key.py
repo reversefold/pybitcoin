7dbd2146b2a0155b2f75ca0f16764fd9b5b4342c
Justin Patrin <papercrane@reversefold.com>
2015-02-14 10:10:26 -0800

    Add a simple generate_key.py

---
+++ generate_key.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from pybitcoin import key
+from pybitcoin import byte_util
+
+
+def main():
+    priv = key.generate_priv()
+    priv_enc = key.encode_privkey(priv)
+    priv_cmp_enc = key.encode_privkey_compressed(priv)
+    pub = key.priv_to_pub(priv)
+    addr = byte_util.base58_encode(key.pub_to_address(pub))
+    addr_comp = byte_util.base58_encode(key.pub_to_address_compressed(pub))
+    print 'Address: %s\nPrivate Key Encoded: %s\nAddress (compressed): %s\nPrivate Key Encoded (compressed): %s' % (addr, priv_enc, addr_comp, priv_cmp_enc)
+
+
+if __name__ == '__main__':
+    main()
